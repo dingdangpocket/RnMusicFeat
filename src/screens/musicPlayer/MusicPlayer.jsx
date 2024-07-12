1ab1ddbf9e8c39716830dae6fc025c77061e6b0c
聂港 <niegang@niegangdeMacBook-Air.local>
2024-07-12 14:00:13 +0800

    code clean

--- src/screens/musicPlayer/MusicPlayer.jsx
+++ src/screens/musicPlayer/MusicPlayer.jsx
@@ -6,59 +6,39 @@ import {
   StyleSheet,
   Dimensions,
   Image,
-  ScrollView,
   Animated,
-  Easing,
   PanResponder,
 } from 'react-native';
 import {ContentContext} from '../../context/ContextProvider';
 import Video from 'react-native-video';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import {Next, Last} from '../../icons/index';
-import {Svg, Line, Path, Circle, Marker} from 'react-native-svg';
-import {log} from 'react-native-reanimated';
+import {Svg, Line, Circle} from 'react-native-svg';
 
 const screenWidth = Dimensions.get('window').width;
 const screenHeight = Dimensions.get('window').height;
 const MusicPlayer = ({route}) => {
-  const scrollViewRef = useRef(null);
-
   const {item, musicList} = route.params;
   const {state, dispatch} = useContext(ContentContext);
-  //   console.log(params);
   const refPlayer = useRef(null);
   const [rate] = useState(1.0);
   const [volume, setVolume] = useState(1.0);
   const [resizeMode, setResizeMode] = useState('contain');
   const [paused, setPaused] = useState(false);
-  const [currentState, setCurrentState] = useState(item.index);
   const [currentSong, setCurrentSong] = useState(musicList[item.index]);
   const [loactMusicList, setLoactMusicList] = useState(musicList);
   const [currentTime, setCurrentTime] = useState();
   const [currentLyrics, setCurrentLyrics] = useState('');
   const [durationTime, setDurationTime] = useState();
-  useEffect(() => {
-    console.log('item', item);
-    console.log('musicList', musicList);
-    console.log(musicList[item.index]);
-    // setCurrentState(item.index);
-    // setCurrentSong(musicList[item.index]);
-    // setLoactMusicList(musicList);
-    console.log('?', currentSong);
-  }, [route, musicList, currentSong, currentState]);
-
-  const onError = onError => {
-    console.log('播放错误', onError);
-  };
   const formatTimeSt = seconds => {
     const minutes = Math.floor(seconds / 60);
     const remainingSeconds = Math.floor(seconds % 60);
-
-    // 将分钟和秒数转换为两位字符串
     const formattedMinutes = String(minutes).padStart(2, '0');
     const formattedSeconds = String(remainingSeconds).padStart(2, '0');
-
     return `${formattedMinutes}:${formattedSeconds}`;
+  };
+  const onError = onError => {
+    console.log('播放错误', onError);
   };
   const onLoad = onLoad => {
     if (onLoad) {
@@ -79,15 +59,13 @@ const MusicPlayer = ({route}) => {
   const loadStart = loadStart => {
     console.log('开始加载', loadStart);
   };
-
   const onEnd = onEnd => {
     console.log('加载结束', onEnd);
   };
   const onSeek = timeInSeconds => {
-    console.log('durationTime', timeInSeconds);
     refPlayer.current.seek(timeInSeconds);
   };
-  const left = () => {
+  const onLastSong = () => {
     let index = loactMusicList.indexOf(currentSong);
     if (index == 0) {
       setCurrentSong(loactMusicList[loactMusicList.length - 1]);
@@ -95,7 +73,7 @@ const MusicPlayer = ({route}) => {
       setCurrentSong(loactMusicList[index - 1]);
     }
   };
-  const right = () => {
+  const onNextSong = () => {
     let index = loactMusicList.indexOf(currentSong);
     if (index == loactMusicList.length - 1) {
       setCurrentSong(loactMusicList[0]);
@@ -106,45 +84,27 @@ const MusicPlayer = ({route}) => {
   const onChangeStatue = () => {
     paused ? setPaused(false) : setPaused(true);
   };
-  const formatTime = seconds => {
-    const minutes = Math.floor(seconds / 60);
-    const remainingSeconds = Math.floor(seconds % 60);
-
-    // 将分钟和秒数转换为两位字符串
-    const formattedMinutes = String(minutes).padStart(2, '0');
-    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
-
-    return `${formattedSeconds}`;
-  };
-
   const onProgress = onProgress => {
     if (
       String(formatTimeSt(onProgress.currentTime)) == formatTimeSt(durationTime)
     ) {
-      right();
+      onNextSong();
     }
     setCurrentTime(onProgress.currentTime);
-
     currentSong.lyrics.forEach(item => {
       if (String(item.time) == String(formatTimeSt(onProgress.currentTime))) {
         setCurrentLyrics(item.text);
       }
     });
   };
-
   const progress = new Animated.Value(0);
-  const widthInterpolate = progress.interpolate({
-    inputRange: [0, 30],
-    outputRange: [0, screenWidth * 0.6],
-  });
-
   const StrokeDashoffset =
     screenWidth * 0.88 - (currentTime / durationTime) * screenWidth * 0.88;
   //进度偏移值=固定总长-（当前偏移比例*固定总长）
-
   useEffect(() => {
     setPointX(String(screenWidth * 0.88 - StrokeDashoffset + 5));
   }, [StrokeDashoffset]);
+
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
     onPanResponderGrant: (event, gestureState) => {},
@@ -153,10 +113,10 @@ const MusicPlayer = ({route}) => {
       const newX = event.nativeEvent.locationX - gestureState.moveX;
       setPointX(newX);
       if (gestureState.moveX > screenWidth * 0.88) {
-        right();
+        onNextSong();
       }
       if (gestureState.moveX < 15) {
-        left();
+        onLastSong();
       } else {
         const rate = gestureState.moveX / (screenWidth * 0.88);
         const seekTime = durationTime * rate;
@@ -209,7 +169,7 @@ const MusicPlayer = ({route}) => {
           width: screenWidth * 0.9,
           height: screenHeight * 0.12,
           borderRadius: 10,
-          backgroundColor: 'rgb(10,80,80)',
+          // backgroundColor: 'rgb(10,80,80)',
           justifyContent: 'center',
           alignItems: 'center',
           marginTop: 10,
@@ -240,7 +200,7 @@ const MusicPlayer = ({route}) => {
               y1="10"
               x2={screenWidth * 0.88}
               y2="10"
-              stroke="rgb(50,50,50)"
+              stroke="rgb(180,180,180)"
               strokeWidth="5"
             />
             <Line
@@ -248,7 +208,7 @@ const MusicPlayer = ({route}) => {
               y1="10"
               x2={String(screenWidth * 0.88)}
               y2="10"
-              stroke="red"
+              stroke="rgb(240,240,240)"
               strokeWidth="3"
               strokeDasharray={String(screenWidth * 0.88)}
               strokeDashoffset={String(StrokeDashoffset)}
@@ -258,13 +218,12 @@ const MusicPlayer = ({route}) => {
               cx={pointX}
               cy="10"
               r="10"
-              fill="rgb(255,255,255)"
+              fill="rgb(220,220,220)"
             />
             <Animated.View {...panResponder.panHandlers}></Animated.View>
           </Svg>
         </View>
       </View>
-
       <View
         style={{
           width: screenWidth * 0.9,
@@ -288,7 +247,7 @@ const MusicPlayer = ({route}) => {
           flexDirection: 'row',
         }}>
         <TouchableOpacity
-          onPress={left}
+          onPress={onLastSong}
           style={{
             backgroundColor: 'white',
             height: screenHeight * 0.07,
@@ -324,7 +283,7 @@ const MusicPlayer = ({route}) => {
           </View>
         </TouchableOpacity>
         <TouchableOpacity
-          onPress={right}
+          onPress={onNextSong}
           style={{
             backgroundColor: 'white',
             height: screenHeight * 0.07,
@@ -333,25 +292,25 @@ const MusicPlayer = ({route}) => {
             justifyContent: 'center',
             alignItems: 'center',
           }}>
-          <Next width="30%" height="30%" onPress={right} />
+          <Next width="30%" height="30%" onPress={onNextSong} />
         </TouchableOpacity>
       </View>
       <Video
         source={{
           uri: currentSong.trackUrl,
         }}
-        ref={refPlayer} //实例;
-        style={styles.container} //样式;
-        rate={rate} //倍率;
-        paused={paused} // 控制暂停/播放，0 代表暂停paused, 1代表播放normal;
-        volume={volume} // 0静音, 1正常，其他数字表示放大倍数;
-        muted={false} // true静音，默认false;
-        onLoad={onLoad} // 加载完毕时回调;
-        onLoadStart={loadStart} // 视频开始加载回调;
-        onProgress={onProgress} // 进度实时回调;
-        onEnd={onEnd} // 视频播放完毕回调函数;
-        repeat={false} //重复播放;
-        resizeMode={resizeMode} //嵌套方式;
+        ref={refPlayer}
+        style={styles.container}
+        rate={rate}
+        paused={paused}
+        volume={volume}
+        muted={false}
+        onLoad={onLoad}
+        onLoadStart={loadStart}
+        onProgress={onProgress}
+        onEnd={onEnd}
+        repeat={false}
+        resizeMode={resizeMode}
         onError={onError}
       />
     </View>
